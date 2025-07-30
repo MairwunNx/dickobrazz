3655f5db4b349e84d6e72073760a91c90bab35c7
Pavel Erokhin <mairwunnx@gmail.com>
2025-07-30 17:49:33 +0300

    Добавил немного магии в консоль! Теперь текст о гонках и сезонах коков выводится в реальном времени, как будто коки сами решили поделиться своими креативными мыслями. Меньше скуки, больше веселья! 🎉🍆

--- application/queries.go
+++ application/queries.go
@@ -7,6 +7,7 @@ import (
 	"dickobot/application/geo"
 	"dickobot/application/logging"
 	"dickobot/application/timings"
+	"fmt"
 	"sort"
 	"strings"
 
@@ -93,6 +94,7 @@ func (app *Application) InlineQueryCockRace(log *logging.Logger, query *tgbotapi
 	}
 	
 	text := app.GenerateCockRaceScoreboard(log, query.From.ID, cocks, seasonStartDate)
+	fmt.Println(text)
 	return InitializeInlineQuery("Гонка коков", text)
 }
 
@@ -198,6 +200,8 @@ func (app *Application) InlineQueryCockSeason(log *logging.Logger, query *tgbota
 	}
 	
 	text := NewMsgCockSeasonsFullText(seasons, totalSeasonsCount, getSeasonWinners)
+	
+	fmt.Println(text)
 	return InitializeInlineQuery("Сезоны коков", text)
 }
 
