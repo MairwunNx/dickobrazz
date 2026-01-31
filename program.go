4af541eb4554be44789e7966f516c61b4521ac55
MairwunNx <personal@mairwunnx.com>
2026-01-31 21:22:10 +0300

    🗑️ Удаление сервера и связанного кода из main.go

--- program.go
+++ program.go
@@ -1,29 +1,15 @@
 package main
 
 import (
-	"context"
 	"dickobrazz/application"
-	"dickobrazz/server"
-	"log"
-	"time"
 )
 
 func main() {
 	app := application.NewApplication()
-	srv, err := server.New()
-	if err != nil {
-		log.Fatalf("Failed to initialize server: %v", err)
-	}
 
 	defer func() {
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
-		if err := srv.Shutdown(ctx); err != nil {
-			log.Printf("Server shutdown error: %v", err)
-		}
 		app.Shutdown()
 	}()
 
-	srv.Start()
 	app.Run()
 }
