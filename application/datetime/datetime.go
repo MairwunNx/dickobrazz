1efed4c360eda4551b065313a148a8d50b09cdb8
MairwunNx <MairwunNx@gmail.com>
2026-02-14 22:49:43 +0300

    ✨ Добавление функций для работы с датами: реализованы парсинг и форматирование дат в МСК

--- application/datetime/datetime.go
+++ application/datetime/datetime.go
@@ -8,6 +8,35 @@ func NowLocation() *time.Location {
 }
 
 func NowTime() time.Time {
-	location, _ := time.LoadLocation("Europe/Moscow")
-	return time.Now().In(location)
+	return time.Now().In(NowLocation())
+}
+
+// ParseUTC парсит дату из бэкэнда (формат 2026-02-11T21:00:00.000Z) и конвертирует в МСК
+func ParseUTC(s string) (time.Time, error) {
+	t, err := time.Parse(time.RFC3339, s)
+	if err != nil {
+		t, err = time.Parse("2006-01-02T15:04:05.000Z", s)
+		if err != nil {
+			return time.Time{}, err
+		}
+	}
+	return t.In(NowLocation()), nil
+}
+
+// FormatDateMSK парсит UTC-дату с бэкэнда и форматирует в "02.01.2006" по МСК
+func FormatDateMSK(s string) string {
+	t, err := ParseUTC(s)
+	if err != nil {
+		return s
+	}
+	return t.Format("02.01.2006")
+}
+
+// FormatDateShortMSK парсит UTC-дату с бэкэнда и форматирует в "02.01.06" по МСК
+func FormatDateShortMSK(s string) string {
+	t, err := ParseUTC(s)
+	if err != nil {
+		return s
+	}
+	return t.Format("02.01.06")
 }
