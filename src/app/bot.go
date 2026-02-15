5dc921c8214135737c1838425c1b83c7f88433c0
MairwunNx <MairwunNx@gmail.com>
2026-02-15 22:51:42 +0300

    ✨ Добавление нового файла bot.go для инициализации Telegram бота

---
+++ src/app/bot.go
@@ -0,0 +1,44 @@
+package app
+
+import (
+	"dickobrazz/src/shared/config"
+	"dickobrazz/src/shared/logging"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+const (
+	defaultAPIEndpoint = "https://api.telegram.org/bot%s/%s"
+)
+
+func InitializeTelegramBot(log *logging.Logger, cfg *config.Configuration) *tgbotapi.BotAPI {
+	token := cfg.Bot.Tg.Token
+	if token == "" {
+		log.F("Telegram token must be set and non-empty")
+	}
+
+	telegramEnv := cfg.Bot.Tg.Env
+	if telegramEnv == "" {
+		telegramEnv = "production"
+	}
+
+	var apiEndpoint string
+	switch telegramEnv {
+	case "test":
+		apiEndpoint = "https://api.telegram.org/bot%s/test/%s"
+		log.I("Using Telegram TEST API endpoint")
+	case "production":
+		apiEndpoint = defaultAPIEndpoint
+		log.I("Using Telegram PRODUCTION API endpoint")
+	default:
+		log.F("Invalid TELEGRAM_ENV value", "telegram_env", telegramEnv)
+	}
+
+	bot, err := tgbotapi.NewBotAPIWithAPIEndpoint(token, apiEndpoint)
+	if err != nil {
+		log.F("Failed to initialize Telegram Bot API", logging.InnerError, err)
+	}
+
+	log.I("Successfully connected to Telegram Bot API!", "environment", telegramEnv)
+	return bot
+}
