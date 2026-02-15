3c169d11f35d3a8bff3192b96c99bdaa58876744
MairwunNx <MairwunNx@gmail.com>
2026-02-15 20:48:04 +0300

    ✨ Добавление локализации: создан новый файл keys.go с ключами для локализации приложения

---
+++ src/shared/localization/keys.go
@@ -0,0 +1,131 @@
+package localization
+
+const (
+	CommonDots = "CommonDots"
+
+	MsgCockScoreboardNotFound = "MsgCockScoreboardNotFound"
+
+	MsgCockSize                    = "MsgCockSize"
+	MsgCockRulerScoreboardDefault  = "MsgCockRulerScoreboardDefault"
+	MsgCockRulerScoreboardSelected = "MsgCockRulerScoreboardSelected"
+	MsgCockRaceScoreboardDefault   = "MsgCockRaceScoreboardDefault"
+	MsgCockRaceScoreboardSelected  = "MsgCockRaceScoreboardSelected"
+
+	MsgCockRulerContextDefault   = "MsgCockRulerContextDefault"
+	MsgCockRulerContextSelected  = "MsgCockRulerContextSelected"
+	MsgCockRaceContextDefault    = "MsgCockRaceContextDefault"
+	MsgCockRaceContextSelected   = "MsgCockRaceContextSelected"
+	MsgCockLadderContextDefault  = "MsgCockLadderContextDefault"
+	MsgCockLadderContextSelected = "MsgCockLadderContextSelected"
+
+	MsgCockLadderScoreboardDefault  = "MsgCockLadderScoreboardDefault"
+	MsgCockLadderScoreboardSelected = "MsgCockLadderScoreboardSelected"
+
+	MsgCockRulerScoreboardTemplate              = "MsgCockRulerScoreboardTemplate"
+	MsgCockRulerScoreboardWinnersTemplate       = "MsgCockRulerScoreboardWinnersTemplate"
+	MsgCockRulerScoreboardTemplateNoDesc        = "MsgCockRulerScoreboardTemplateNoDesc"
+	MsgCockRulerScoreboardWinnersTemplateNoDesc = "MsgCockRulerScoreboardWinnersTemplateNoDesc"
+
+	MsgCockRaceScoreboardTemplate              = "MsgCockRaceScoreboardTemplate"
+	MsgCockRaceScoreboardWinnersTemplate       = "MsgCockRaceScoreboardWinnersTemplate"
+	MsgCockRaceScoreboardTemplateNoDesc        = "MsgCockRaceScoreboardTemplateNoDesc"
+	MsgCockRaceScoreboardWinnersTemplateNoDesc = "MsgCockRaceScoreboardWinnersTemplateNoDesc"
+
+	MsgCockLadderScoreboardTemplate              = "MsgCockLadderScoreboardTemplate"
+	MsgCockLadderScoreboardWinnersTemplate       = "MsgCockLadderScoreboardWinnersTemplate"
+	MsgCockLadderScoreboardTemplateNoDesc        = "MsgCockLadderScoreboardTemplateNoDesc"
+	MsgCockLadderScoreboardWinnersTemplateNoDesc = "MsgCockLadderScoreboardWinnersTemplateNoDesc"
+
+	MsgCockRaceFooterActiveSeason = "MsgCockRaceFooterActiveSeason"
+	MsgCockRaceFooterNoSeason     = "MsgCockRaceFooterNoSeason"
+
+	MsgCockAchievementsTemplate           = "MsgCockAchievementsTemplate"
+	MsgCockAchievementsTemplateOtherPages = "MsgCockAchievementsTemplateOtherPages"
+
+	InlineTitleCockDynamic = "InlineTitleCockDynamic"
+	InlineTitleCockSeason  = "InlineTitleCockSeason"
+
+	DescCockDynamic           = "DescCockDynamic"
+	DescCockSeason            = "DescCockSeason"
+	MsgSeasonUnknownStartDate = "MsgSeasonUnknownStartDate"
+	MsgSeasonButton           = "MsgSeasonButton"
+	MsgSeasonNotFound         = "MsgSeasonNotFound"
+	MsgCallbackInvalidFormat  = "MsgCallbackInvalidFormat"
+	MsgCallbackParseError     = "MsgCallbackParseError"
+
+	MsgUserPullingRecently = "MsgUserPullingRecently"
+	MsgUserPullingSince    = "MsgUserPullingSince"
+	MsgListSeparator       = "MsgListSeparator"
+	MsgListSeparatorLast   = "MsgListSeparatorLast"
+
+	UnitDay            = "UnitDay"
+	UnitMonth          = "UnitMonth"
+	UnitYear           = "UnitYear"
+	UnitSeason         = "UnitSeason"
+	UnitSeasonGenitive = "UnitSeasonGenitive"
+	UnitHour           = "UnitHour"
+	UnitMinute         = "UnitMinute"
+
+	UptimeDayShort    = "UptimeDayShort"
+	UptimeHourShort   = "UptimeHourShort"
+	UptimeMinuteShort = "UptimeMinuteShort"
+
+	MsgUnknownValue = "MsgUnknownValue"
+
+	AnonymousNameTemplate = "AnonymousNameTemplate"
+
+	MsgHelpText         = "MsgHelpText"
+	MsgHidePrompt       = "MsgHidePrompt"
+	MsgHideStatusHidden = "MsgHideStatusHidden"
+	MsgHideButtonHide   = "MsgHideButtonHide"
+	MsgHideButtonShow   = "MsgHideButtonShow"
+
+	MsgAchievementCompleted    = "MsgAchievementCompleted"
+	MsgAchievementInProgress   = "MsgAchievementInProgress"
+	MsgAchievementNotCompleted = "MsgAchievementNotCompleted"
+
+	LuckLabelGodRandom      = "LuckLabelGodRandom"
+	LuckLabelCosmicLuck     = "LuckLabelCosmicLuck"
+	LuckLabelFairyLuck      = "LuckLabelFairyLuck"
+	LuckLabelSuperLuck      = "LuckLabelSuperLuck"
+	LuckLabelIncredibleLuck = "LuckLabelIncredibleLuck"
+	LuckLabelVeryLucky      = "LuckLabelVeryLucky"
+	LuckLabelLucky          = "LuckLabelLucky"
+	LuckLabelBalanced       = "LuckLabelBalanced"
+	LuckLabelUnlucky        = "LuckLabelUnlucky"
+	LuckLabelBad            = "LuckLabelBad"
+	LuckLabelGloom          = "LuckLabelGloom"
+	LuckLabelHellTilt       = "LuckLabelHellTilt"
+	LuckLabelBurningInHell  = "LuckLabelBurningInHell"
+
+	VolatilityLabelStone        = "VolatilityLabelStone"
+	VolatilityLabelStable       = "VolatilityLabelStable"
+	VolatilityLabelModerate     = "VolatilityLabelModerate"
+	VolatilityLabelLivelySpread = "VolatilityLabelLivelySpread"
+	VolatilityLabelUneven       = "VolatilityLabelUneven"
+	VolatilityLabelChaotic      = "VolatilityLabelChaotic"
+	VolatilityLabelRandom       = "VolatilityLabelRandom"
+
+	IrkLabelZero      = "IrkLabelZero"
+	IrkLabelMinimal   = "IrkLabelMinimal"
+	IrkLabelVerySmall = "IrkLabelVerySmall"
+	IrkLabelSmall     = "IrkLabelSmall"
+	IrkLabelReduced   = "IrkLabelReduced"
+	IrkLabelAverage   = "IrkLabelAverage"
+	IrkLabelIncreased = "IrkLabelIncreased"
+	IrkLabelLarge     = "IrkLabelLarge"
+	IrkLabelVeryLarge = "IrkLabelVeryLarge"
+	IrkLabelMaximum   = "IrkLabelMaximum"
+	IrkLabelUltimate  = "IrkLabelUltimate"
+
+	GrowthSpeedLabelCosmic   = "GrowthSpeedLabelCosmic"
+	GrowthSpeedLabelExtreme  = "GrowthSpeedLabelExtreme"
+	GrowthSpeedLabelVeryFast = "GrowthSpeedLabelVeryFast"
+	GrowthSpeedLabelFast     = "GrowthSpeedLabelFast"
+	GrowthSpeedLabelModerate = "GrowthSpeedLabelModerate"
+	GrowthSpeedLabelAverage  = "GrowthSpeedLabelAverage"
+	GrowthSpeedLabelSlow     = "GrowthSpeedLabelSlow"
+	GrowthSpeedLabelVerySlow = "GrowthSpeedLabelVerySlow"
+	GrowthSpeedLabelTurtle   = "GrowthSpeedLabelTurtle"
+	GrowthSpeedLabelStalled  = "GrowthSpeedLabelStalled"
+)
