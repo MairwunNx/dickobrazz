69692ccc21fb0f5661fa79f2f93874347c35cfaf
MairwunNx <MairwunNx@gmail.com>
2026-02-15 20:46:59 +0300

    ✨ Добавление функционала: создан новый файл errors.go для обработки ошибок API

---
+++ src/shared/api/errors.go
@@ -0,0 +1,13 @@
+package api
+
+import "fmt"
+
+// APIError представляет ошибку от API
+type APIError struct {
+	StatusCode int
+	Message    string
+}
+
+func (e *APIError) Error() string {
+	return fmt.Sprintf("API error %d: %s", e.StatusCode, e.Message)
+}
