2e4b962429c25cc494276954f182d1e6de42f2cc
MairwunNx <MairwunNx@gmail.com>
2026-02-15 20:51:01 +0300

    ✨ Добавление функционала: создан новый файл markdown.go для экранирования Markdown-текста

---
+++ src/shared/formatting/markdown.go
@@ -0,0 +1,15 @@
+package formatting
+
+import "strings"
+
+func EscapeMarkdownV2(input string) string {
+	var str strings.Builder
+	escapable := "_*[]()~`>#+-=|{}.!\\"
+	for _, char := range input {
+		if strings.ContainsRune(escapable, char) {
+			str.WriteRune('\\')
+		}
+		str.WriteRune(char)
+	}
+	return str.String()
+}
