67a14fca4c29e900d5f03988a6e033500cf6f234
MairwunNx <MairwunNx@gmail.com>
2026-02-15 20:46:27 +0300

    ✨ Добавление функционала: создан новый файл tracing.go для отчетности о времени выполнения функций

---
+++ src/shared/timings/tracing.go
@@ -0,0 +1,25 @@
+package timings
+
+import (
+	"dickobrazz/src/shared/logging"
+	"time"
+)
+
+func ReportExecutionForResultError[T any, E error](log *logging.Logger, action func() (T, E), report func(l *logging.Logger)) (T, E) {
+	start := time.Now()
+	result, err := action()
+	report(log.With(logging.ExecutionTime, time.Since(start).String()))
+	return result, err
+}
+
+func ReportExecutionForResult[T any](log *logging.Logger, action func() T, report func(l *logging.Logger)) T {
+	start, result := time.Now(), action()
+	report(log.With(logging.ExecutionTime, time.Since(start).String()))
+	return result
+}
+
+func ReportExecution(log *logging.Logger, action func(), report func(l *logging.Logger)) {
+	start := time.Now()
+	action()
+	report(log.With(logging.ExecutionTime, time.Since(start).String()))
+}
