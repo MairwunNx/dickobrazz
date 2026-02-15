87c50497fa3224518aa3365c2d0b6e9d057f9700
MairwunNx <MairwunNx@gmail.com>
2026-02-15 20:45:00 +0300

    ✨ Добавление логирования: создан новый файл logger.go с реализацией логирования

---
+++ src/shared/logging/logger.go
@@ -0,0 +1,63 @@
+package logging
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	ExecutionTime = "exe_time"
+	SysVersion    = "sys_version"
+	SysBuildRv    = "sys_buildrv"
+	SysBuildAt    = "sys_buildat"
+	SysRuntime    = "sys_runtime"
+	InnerError    = "inner_error"
+	UserId        = "user_id"
+	UserName      = "user_name"
+	QueryId       = "query_id"
+	QueryType     = "query_type"
+	ChatType      = "chat_type"
+	ChatId        = "chat_id"
+	RndSource     = "rnd_source"
+)
+
+type Logger struct {
+	log *slog.Logger
+}
+
+func NewLogger() *Logger {
+	logger := slog.New(slog.NewJSONHandler(os.Stderr, nil)).With(
+		SysVersion, Version,
+		SysBuildRv, BuildRv,
+		SysBuildAt, BuildAt,
+		SysRuntime, GoVersion,
+	)
+	slog.SetDefault(logger)
+	logger.Info("Starting dickbot (dickobrazz) ...")
+	return &Logger{log: logger}
+}
+
+func (l *Logger) With(args ...any) *Logger {
+	return &Logger{log: l.log.With(args...)}
+}
+
+func (l *Logger) D(msg string, args ...any) {
+	l.log.Debug(msg, args...)
+}
+
+func (l *Logger) I(msg string, args ...any) {
+	l.log.Info(msg, args...)
+}
+
+func (l *Logger) W(msg string, args ...any) {
+	l.log.Warn(msg, args...)
+}
+
+func (l *Logger) E(msg string, args ...any) {
+	l.log.Error(msg, args...)
+}
+
+func (l *Logger) F(msg string, args ...any) {
+	l.log.Error(msg, args...)
+	panic(msg)
+}
